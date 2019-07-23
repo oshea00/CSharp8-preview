17f842f1d0ce1336554df48b60720aeaf73d19be
Mike OShea <oshea00@gmail.com>
2019-07-23 16:49:43 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace preview
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 1. query syntax - looks the same. Note await location.
+            var res = from l in getLines() where l.StartsWith("#") select l;
+            await foreach(var line in res) {
+                System.Console.WriteLine(line);
+            }
+            System.Console.WriteLine("Done 1!");
+
+            // 2. Using fluent with async execution. Note await location.
+             var ares = await getLines().Where(l=>l.StartsWith("#")).Select(l=>l)
+                                .ToListAsync();
+            foreach(var line in ares) {
+                System.Console.WriteLine(line);
+            }
+            System.Console.WriteLine("Done 2!");
+
+            // 3. Using fluent with await on the iterator - Note await location.
+            res = getLines().Where(l=>l.StartsWith("#")).Select(l=>l);
+            await foreach(var line in res) {
+                System.Console.WriteLine(line);
+            }
+            System.Console.WriteLine("Done 3!");
+        }
+
+        static async IAsyncEnumerable<string> getLines() {
+            var rdr = new StreamReader(@"c:\users\mike\repos\fintools\readme.md");
+            while (!rdr.EndOfStream) {
+                var line = await rdr.ReadLineAsync();
+                yield return line;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# C# 8.0 Preview - IAsyncEnumerable and Linq
+
+Short demo of the new IAsyncEnumerable feature in C# 8.0.
+
+## Project setup for new projects
+
+* Install the latest .Net Core 3.0 preview.
+* In Visual Studio 2019, allow preview features under: Tools->Options->Environment->Preview Features
+* VSCode needs no changes.
+
+## Additional steps for new projects
+
+* csproj file for new projects should have LangVersion tag for C# 8.0
+```
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>netcoreapp3.0</TargetFramework>
+    <LangVersion>8.0</LangVersion>
+  </PropertyGroup>
+```
+* Install system.linq.async package to project. Example:
+dotnet add package System.Linq.Async -v 4.0.0-preview.6.build.801
+
